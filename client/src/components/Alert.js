570f2cb732b58975e1d92f12ca5b05e2551a7024
bartek dec <decbartek@gmail.com>
2023-02-05 19:54:59 +0100

    created Alert component

---
+++ client/src/components/Alert.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import {useSelector} from "react-redux";
+
+const Alert = () => {
+    const {alertText} = useSelector((state) => state.register);
+
+    return (
+        <Wrapper>
+            <h3 className='alert'>{alertText}</h3>
+        </Wrapper>
+    );
+};
+
+export default Alert;
+
+const Wrapper = styled.div`
+  width: 100%;
+  margin-bottom: 1rem;
+  border: 1px solid var(--Error-Color);
+  border-radius: var(--border-radius-4);
+  padding: 0.75rem 1rem;
+
+  .alert {
+    text-align: center;
+    font-size: var(--font-size-15);
+    font-weight: var(--font-weight-5);
+    color: var(--Error-Color);
+    letter-spacing: 1px;
+  }
+`;
