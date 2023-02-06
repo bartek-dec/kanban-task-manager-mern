095c5bfeda1485a97b96646856468fca7be7d44d
bartek dec <decbartek@gmail.com>
2023-02-06 12:14:50 +0100

    created notFound middleware

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('server running');
+import express from 'express';
+
+// middleware
+import notFoundMiddleware from "./middleware/notFound.js";
+
+const app = express();
+app.use(express.json());
+
+//fake home route
+app.get('/', (req, res) => {
+    res.send('welcone');
+});
+
+// catch errors
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log('server is running');
+})
