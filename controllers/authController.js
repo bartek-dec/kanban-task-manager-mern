c5a0c8531f75e5de6fee18fb5b84dfdc8f216cbb
bartek dec <decbartek@gmail.com>
2023-02-06 13:38:22 +0100

    created authController file

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+export const register = async (req, res) => {
+    return res.send('register user');
+}
+
+export const login = async (req, res) => {
+    return res.send('login user');
+}
+
+export const updateUser = async (req, res) => {
+    return res.send('update user');
+}
