5dd1f2917d323abe6fe1fc8b51a5a6dfddd2fa86
bartek dec <decbartek@gmail.com>
2023-02-06 22:44:51 +0100

    created unAuthenticatedError

--- errors/index.js
+++ errors/index.js
@@ -1,4 +1,5 @@
 import NotFoundError from "./notFoundError.js";
 import BadRequestError from "./badRequestError.js";
+import UnAuthenticatedError from "./unAuthenticatedError.js";
 
-export {NotFoundError, BadRequestError}
+export {NotFoundError, BadRequestError, UnAuthenticatedError}
