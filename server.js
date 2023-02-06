64d926f7bbece19d6b436abd7a1b6da2106755be
bartek dec <decbartek@gmail.com>
2023-02-06 22:40:45 +0100

    installed morgan package

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import 'express-async-errors';
 import connectDB from "./db/connect.js";
+import morgan from 'morgan';
 
 // middleware
 import notFoundMiddleware from "./middleware/notFound.js";
@@ -11,6 +12,10 @@ import authRouter from "./routes/authRouter.js";
 const app = express();
 app.use(express.json());
 dotenv.config();
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'));
+}
 
 //fake home route
 app.get('/', (req, res) => {
