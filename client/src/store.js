06867152d66adea8e94779d05e3b76e89e7cbfd6
bartek dec <decbartek@gmail.com>
2023-02-08 19:15:36 +0100

    created themeSlice with change theme functionality

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import registerLoginSlice from "./features/registerLogin/registerLoginSlice";
 import boardSlice from "./features/board/boardSlice";
+import themeSlice from "./features/theme/themeSlice";
 
 const store = configureStore({
     reducer: {
         register: registerLoginSlice,
-        board: boardSlice
+        board: boardSlice,
+        theme: themeSlice
     }
 });
 
