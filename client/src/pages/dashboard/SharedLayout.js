89ab3813e13686ea1ab59ff7a95e65446217a112
bartek dec <decbartek@gmail.com>
2023-02-08 17:35:25 +0100

    modified layout in the SharedLayout

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,15 +1,26 @@
-import {Link, Outlet} from "react-router-dom";
+import {Outlet} from "react-router-dom";
+import styled from "styled-components";
+import {Sidebar, Navbar} from '../../components';
 
 const SharedLayout = () => {
     return (
-        <div>
-            <nav>
-                <Link to='/add-board'>add board</Link>
-                <Link to='/profile'>profile</Link>
-            </nav>
-            <Outlet/>
-        </div>
+        <Wrapper>
+            <main className='dashboard'>
+                <Sidebar/>
+                <div>
+                    <Navbar/>
+                    <Outlet/>
+                </div>
+            </main>
+        </Wrapper>
     );
 };
 
 export default SharedLayout;
+
+const Wrapper = styled.section`
+  .dashboard {
+    display: grid;
+    grid-template-columns:auto 1fr;
+  }
+`;
