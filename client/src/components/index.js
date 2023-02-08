01ac8873e048dbc5d1fe7db913010b21eab4da75
bartek dec <decbartek@gmail.com>
2023-02-08 17:34:08 +0100

    created BoardListItem component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,8 @@
 import Logo from "./Logo";
 import FormInput from "./FormInput";
 import Alert from "./Alert";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+import BoardListItem from "./BoardListItem";
 
-export {Logo, FormInput, Alert}
+export {Logo, FormInput, Alert, Sidebar, Navbar, BoardListItem}
