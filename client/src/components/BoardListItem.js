ad4de766021956d3d065f2cb3a791c1480b1ebd3
bartek dec <decbartek@gmail.com>
2023-02-09 20:03:03 +0100

    refactor

--- client/src/components/BoardListItem.js
+++ client/src/components/BoardListItem.js
@@ -13,11 +13,10 @@ const BoardListItem = ({title, id}) => {
     }
 
     return (
-        <Wrapper data-id={id} type='button' onClick={handleClick}>
-            <div  className={activeBtn === id ? 'btn-container active' : 'btn-container'}>
-                <TbLayoutBoardSplit className='icon-img'/>
-                <h4 className='name'>{title}</h4>
-            </div>
+        <Wrapper data-id={id} type='button' className={activeBtn === id ? 'board-btn active' : 'board-btn'}
+                 onClick={handleClick}>
+            <TbLayoutBoardSplit className='icon-img'/>
+            <h4>{title}</h4>
         </Wrapper>
     );
 };
@@ -25,42 +24,7 @@ const BoardListItem = ({title, id}) => {
 export default BoardListItem;
 
 const Wrapper = styled.button`
-  border: none;
-  width: 100%;
-  background-color: transparent;
-
-  .btn-container {
-    padding: 0.85rem;
-    padding-left: 2rem;
-    text-transform: capitalize;
-    font-size: var(--font-size-15);
-    border-top-right-radius: var(--border-radius-24);
-    border-bottom-right-radius: var(--border-radius-24);
-    color: var(--Medium-Grey);
-    cursor: pointer;
-
-    display: flex;
-    align-items: center;
-  }
-
-  .btn-container:hover {
-    background-color: var(--Light-Grey-Light-BG);
+  .board-btn:hover .icon-img {
     color: var(--Main-Purple);
-  }
-
-  .active {
-    background-color: var(--Main-Purple);
-    color: var(--White);
-  }
-
-  .btn-container:hover .icon-img {
-    color: var(--Main-Purple);
-  }
-
-  .icon-img {
-    width: 1.35rem;
-    height: 1.35rem;
-    margin-right: 1rem;
-    stroke-width: 1;
   }
 `;
