60bc6278b7cfe37005675b45c4ec1eb9f315d514
bartek dec <decbartek@gmail.com>
2023-02-10 14:32:31 +0100

    added change size effect at small screen

--- client/src/components/AddTaskButton.js
+++ client/src/components/AddTaskButton.js
@@ -5,7 +5,7 @@ const AddTaskButton = () => {
 
     return (
         <Wrapper>
-            <HiPlusSm className='plus-icon'/> Add New Task
+            <HiPlusSm className='plus-icon'/> <span>Add New Task</span>
         </Wrapper>
     );
 };
@@ -15,11 +15,11 @@ export default AddTaskButton;
 const Wrapper = styled.button`
   display: flex;
   align-items: center;
-  padding: 0.85rem 1.5rem;
+  padding: 0.85rem;
   background-color: var(--Main-Purple);
   color: var(--White);
   border: none;
-  border-radius: var(--border-radius-24);
+  border-radius: 50%;
   font-size: var(--font-size-15);
   font-weight: var(--font-weight-7);
   letter-spacing: 1px;
@@ -31,10 +31,22 @@ const Wrapper = styled.button`
     background-color: var(--Main-Purple-Hover-3);
   }
 
+  span {
+    display: none;
+  }
+
   .plus-icon {
     display: flex;
     align-items: center;
     width: 1rem;
     height: 1rem;
+  }
+
+  @media (min-width: 650px) {
+    padding: 0.85rem 1.5rem;
+    border-radius: var(--border-radius-24);
+    span {
+      display: inline;
+    }
   }
 `;
