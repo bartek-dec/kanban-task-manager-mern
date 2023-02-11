10fdd292cb1125de28da751f1c2992660a821a50
bartek dec <decbartek@gmail.com>
2023-02-11 22:20:21 +0100

    added "close sidebar modal" functionality

--- client/src/components/UserButton.js
+++ client/src/components/UserButton.js
@@ -1,9 +1,19 @@
 import {FaUserAlt} from 'react-icons/fa';
 import styled from "styled-components";
+import {useDispatch} from "react-redux";
+import {showUserModal} from "../features/user/userSlice";
+import {closeSidebarModal} from "../features/sidebarSlice/sidebarSlice";
 
 const UserButton = () => {
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+        dispatch(showUserModal());
+        dispatch(closeSidebarModal());
+    }
+
     return (
-        <Wrapper type='button' className='board-btn'>
+        <Wrapper type='button' className='board-btn' onClick={handleClick}>
             <FaUserAlt className='icon-img'/>
             <h4>User Settings</h4>
         </Wrapper>
