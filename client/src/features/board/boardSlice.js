afdc706f0afb3cb39b52bf441e7ef7b2e5eb07f2
bartek dec <decbartek@gmail.com>
2023-02-15 10:09:06 +0100

    added functionality to show "create board" modal component

--- client/src/features/board/boardSlice.js
+++ client/src/features/board/boardSlice.js
@@ -2,6 +2,10 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
     isEditBoardModalVisible: false,
+    isCreateBoardModalVisible: false,
+    showAlert: false,
+    isLoading: false,
+    alertText: '',
     activeBtn: 0,
     boards: ['platform launch', 'marketing plan', 'roadmap']
 }
@@ -16,12 +20,25 @@ const boardSlice = createSlice({
         closeEditModal: (state) => {
             state.isEditBoardModalVisible = false;
         },
+        showCreateModal: (state) => {
+            state.isCreateBoardModalVisible = true;
+        },
+        closeCreateModal: (state) => {
+            state.isCreateBoardModalVisible = false;
+        },
         setIsActive: (state, action) => {
             state.activeBtn = action.payload;
-        }
+        },
+        setAlert: (state, action) => {
+            state.showAlert = action.payload;
+        },
+        setAlertText: (state, action) => {
+            state.alertText = action.payload;
+        },
+
     }
 });
 
 export default boardSlice.reducer;
 
-export const {showEditModal, closeEditModal, setIsActive} = boardSlice.actions;
+export const {showEditModal, closeEditModal, showCreateModal, closeCreateModal, setIsActive} = boardSlice.actions;
