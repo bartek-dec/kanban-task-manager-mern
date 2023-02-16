0e0d61b058f5c9e3d2970768b87eb3803c868d3a
bartek dec <decbartek@gmail.com>
2023-02-16 22:30:29 +0100

    created Board schema

---
+++ models/Board.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const BoardSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: [true, 'Please provide a name']
+        },
+        columns: {
+            type: [String]
+        },
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'Please provide a user']
+        },
+    },
+    {timestamps: true}
+);
+
+export default mongoose.model('Board', BoardSchema);
