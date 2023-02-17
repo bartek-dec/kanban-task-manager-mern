9e1a8baf3c8692a7ded8b85ff05a0c67d747ad98
bartek dec <decbartek@gmail.com>
2023-02-17 11:49:16 +0100

    created axios util file with common logic related to authorized requests

---
+++ client/src/utils/axios.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import {getUserFromLocalStorage, removeUserFromLocalStorage} from "./localStorage";
+import {setAlertText, setShowAlert, setToken, setUser} from "../features/user/userSlice";
+
+export const authFetch = axios.create({
+    baseURL: '/api/v1',
+});
+
+authFetch.interceptors.request.use((config) => {
+    const {token} = getUserFromLocalStorage();
+    if (token) {
+        config.headers['Authorization'] = `Bearer${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+export const checkForUnAuthorizedError = (error, thunkAPI) => {
+    if (error.response.status === 401) {
+        thunkAPI.dispatch(setShowAlert(true));
+        thunkAPI.dispatch(setAlertText('Unauthorized! Logging out...'));
+        setTimeout(() => {
+            thunkAPI.dispatch(setUser(null));
+            thunkAPI.dispatch(setToken(null));
+            thunkAPI.dispatch(setShowAlert(false));
+            thunkAPI.dispatch(setAlertText(''));
+            removeUserFromLocalStorage();
+        }, 2000);
+    }
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+}
