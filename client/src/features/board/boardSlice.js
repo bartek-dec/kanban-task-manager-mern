117cf7a83f71060dcec91919d7aef9d5bec5c336
bartek dec <decbartek@gmail.com>
2023-02-19 17:26:03 +0100

    added logic in the deleteBoard() function on the front-end and removed unnecessary code

--- client/src/features/board/boardSlice.js
+++ client/src/features/board/boardSlice.js
@@ -4,8 +4,10 @@ import {authFetch, checkForUnAuthorizedError} from "../../utils/axios";
 const initialState = {
     isEditBoardModalVisible: false,
     isCreateBoardModalVisible: false,
+    isDeleteBoardModalVisible: false,
     isLoading: false,
-    activeBtn: 0,
+    activeBoard: '',
+    currentBoardName: '',
     boards: []
 }
 
@@ -14,7 +16,7 @@ export const createBoard = createAsyncThunk('createBoard', async (payload, thunk
         const {data} = await authFetch.post('/boards', payload);
         return data;
     } catch (error) {
-        return checkForUnAuthorizedError(error, thunkAPI, closeCreateModal);
+        return checkForUnAuthorizedError(error, thunkAPI);
     }
 });
 
@@ -22,6 +24,15 @@ export const getBoards = createAsyncThunk('getBoards', async (_, thunkAPI) => {
     try {
         const {data} = await authFetch.get('/boards');
         return data;
+    } catch (error) {
+        return checkForUnAuthorizedError(error, thunkAPI);
+    }
+});
+
+export const deleteBoard = createAsyncThunk('deleteBoard', async (id, thunkAPI) => {
+    try {
+        await authFetch.delete(`boards/${id}`);
+        thunkAPI.dispatch(getBoards());
     } catch (error) {
         return checkForUnAuthorizedError(error, thunkAPI);
     }
@@ -43,18 +54,29 @@ const boardSlice = createSlice({
         closeCreateModal: (state) => {
             state.isCreateBoardModalVisible = false;
         },
-        setIsActive: (state, action) => {
-            state.activeBtn = action.payload;
+        showDeleteModal: (state) => {
+            state.isDeleteBoardModalVisible = true;
         },
+        closeDeleteModal: (state) => {
+            state.isDeleteBoardModalVisible = false;
+        },
+        setActiveBoard: (state, action) => {
+            state.activeBoard = action.payload;
+        },
+        setCurrentBoardName: (state, action) => {
+            state.currentBoardName = action.payload;
+        }
     },
     extraReducers: (builder) => {
         builder.addCase(createBoard.pending, (state) => {
             state.isLoading = true;
         }).addCase(createBoard.fulfilled, (state, action) => {
             state.isLoading = false;
+            state.isCreateBoardModalVisible = false;
             state.boards = [...state.boards, action.payload.board];
         }).addCase(createBoard.rejected, (state) => {
             state.isLoading = false;
+            state.isCreateBoardModalVisible = false;
         }).addCase(getBoards.pending, (state) => {
             state.isLoading = true;
         }).addCase(getBoards.fulfilled, (state, action) => {
@@ -62,6 +84,15 @@ const boardSlice = createSlice({
             state.boards = action.payload.boards;
         }).addCase(getBoards.rejected, (state) => {
             state.isLoading = false;
+        }).addCase(deleteBoard.pending, (state) => {
+            state.isLoading = true;
+        }).addCase(deleteBoard.fulfilled, (state) => {
+            state.isLoading = false;
+            state.activeBoard = '';
+            state.isDeleteBoardModalVisible = false;
+        }).addCase(deleteBoard.rejected, (state) => {
+            state.isLoading = false;
+            state.isDeleteBoardModalVisible = false;
         })
     }
 });
@@ -73,5 +104,8 @@ export const {
     closeEditModal,
     showCreateModal,
     closeCreateModal,
-    setIsActive,
+    showDeleteModal,
+    closeDeleteModal,
+    setActiveBoard,
+    setCurrentBoardName
 } = boardSlice.actions;
