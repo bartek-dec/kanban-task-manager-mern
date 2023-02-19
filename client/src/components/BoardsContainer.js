99736c9cc4605cf7ce8a27a1278afbb39aa19b33
bartek dec <decbartek@gmail.com>
2023-02-19 18:08:30 +0100

    added logic to set current active board

--- client/src/components/BoardsContainer.js
+++ client/src/components/BoardsContainer.js
@@ -2,22 +2,36 @@ import styled from "styled-components";
 import {useSelector, useDispatch} from "react-redux";
 import {BoardListItem, CreateBoardButton} from "./index";
 import {useEffect} from "react";
-import {getBoards} from "../features/board/boardSlice";
+import {getBoards, setCurrentBoardName, setActiveBoard} from "../features/board/boardSlice";
+import {useNavigate} from "react-router-dom";
 
 const BoardsContainer = () => {
-    const {boards} = useSelector((state) => state.board);
+    const {boards, activeBoard} = useSelector((state) => state.board);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
         dispatch(getBoards());
     }, []);
 
+    useEffect(() => {
+        // set active board
+        if (activeBoard) {
+            navigate(`/${activeBoard}`);
+        } else if (!activeBoard && boards.length > 0) {
+            const {_id: id, name} = boards[0];
+            dispatch(setCurrentBoardName(name));
+            dispatch(setActiveBoard(id));
+            navigate(`/${id}`);
+        }
+    }, [boards]);
+
     return (
         <Wrapper>
             <h4 className='boards-header'>all boards ({boards.length})</h4>
             {boards.map((item, index) => {
-                const {name} = item;
-                return <BoardListItem key={index} id={index} title={name}/>
+                const {name, _id: id} = item;
+                return <BoardListItem key={index} id={id} title={name}/>
             })}
             <CreateBoardButton/>
         </Wrapper>
