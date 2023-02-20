f52d77d555edf6afaef908b5162b2ebb6bc7748b
bartek dec <decbartek@gmail.com>
2023-02-20 13:19:06 +0100

    removed alertSlice

--- client/src/store.js
+++ client/src/store.js
@@ -4,7 +4,6 @@ import boardSlice from "./features/board/boardSlice";
 import themeSlice from "./features/theme/themeSlice";
 import sidebarSlice from "./features/sidebarSlice/sidebarSlice";
 import logoutSlice from "./features/logoutSlice/logoutSlice";
-import alertSlice from "./features/alert/alertSlice";
 
 const store = configureStore({
     reducer: {
@@ -13,7 +12,6 @@ const store = configureStore({
         theme: themeSlice,
         sidebar: sidebarSlice,
         logout: logoutSlice,
-        alert: alertSlice
     }
 });
 
