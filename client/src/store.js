c3d312773234a29558ec148d45c64905a9c078b7
bartek dec <decbartek@gmail.com>
2023-02-24 11:12:16 +0100

    created task slice

--- client/src/store.js
+++ client/src/store.js
@@ -4,6 +4,7 @@ import boardSlice from "./features/board/boardSlice";
 import themeSlice from "./features/theme/themeSlice";
 import sidebarSlice from "./features/sidebarSlice/sidebarSlice";
 import logoutSlice from "./features/logoutSlice/logoutSlice";
+import taskSlice from "./features/task/taskSlice";
 
 const store = configureStore({
     reducer: {
@@ -12,6 +13,7 @@ const store = configureStore({
         theme: themeSlice,
         sidebar: sidebarSlice,
         logout: logoutSlice,
+        task: taskSlice
     }
 });
 
