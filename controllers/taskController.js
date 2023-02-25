4c96fd52ffe2648753cf95338011993a26dfd5cf
bartek dec <decbartek@gmail.com>
2023-02-25 22:15:27 +0100

    created basic taskController

---
+++ controllers/taskController.js
@@ -0,0 +1,15 @@
+export const createTask = async (req, res) => {
+    return res.send('create task');
+}
+
+export const getTasks = async (req, res) => {
+    return res.send('get tasks');
+}
+
+export const updateTask = async (req, res) => {
+    return res.send('update task');
+}
+
+export const deleteTask = async (req, res) => {
+    return res.send('delete task');
+}
