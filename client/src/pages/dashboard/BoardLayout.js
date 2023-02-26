b21ba63b402128b685b4ab3346efe7303735f67c
bartek dec <decbartek@gmail.com>
2023-02-26 22:20:25 +0100

    added logic to the BoardLayout component

--- client/src/pages/dashboard/BoardLayout.js
+++ client/src/pages/dashboard/BoardLayout.js
@@ -1,16 +1,31 @@
-import React from 'react';
 import styled from "styled-components";
+import {useSelector} from "react-redux";
+import {Column} from "../../components";
 
 const BoardLayout = () => {
+    const {activeBoard} = useSelector((state) => state.board);
+    const {tasks} = useSelector((state) => state.task);
+
     return (
-        <Wrapper>
-            profile
+        <Wrapper column={activeBoard?.columns.length}>
+            {activeBoard?.columns.map((column, index) => {
+                return (
+                    // column in the board is the same as status in task
+                    <Column key={index} index={index} column={column} tasks={tasks[column]}/>
+                )
+            })}
         </Wrapper>
     );
 };
 
 export default BoardLayout;
 
-const Wrapper = styled.div`
-  background-color: #EA5555;
+const Wrapper = styled.section`
+  min-height: calc(100vh - var(--Navbar-Height));
+  padding: 1.5rem;
+  overflow: auto;
+
+  display: grid;
+  grid-template-columns: repeat(${props => props.column}, 18rem);
+  column-gap: 2rem;
 `;
