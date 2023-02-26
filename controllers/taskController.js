13e1dc0aca381e7b287dfb3842ccacf0657e7e30
bartek dec <decbartek@gmail.com>
2023-02-26 15:23:13 +0100

    added logic to the createTask() function in the taskController

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,5 +1,23 @@
+import Task from "../models/Task.js";
+import Board from "../models/Board.js";
+import {StatusCodes} from "http-status-codes";
+import {BadRequestError, NotFoundError} from "../errors/index.js";
+
 export const createTask = async (req, res) => {
-    return res.send('create task');
+    const {title, status, boardId} = req.body;
+
+    if (!title || !status) {
+        throw new BadRequestError('Please provide all values');
+    }
+
+    const board = await Board.findOne({_id: boardId});
+    if (!board) {
+        throw new NotFoundError(`No board with id: ${boardId}`);
+    }
+
+    const task = await Task.create(req.body);
+
+    return res.status(StatusCodes.CREATED).json({task});
 }
 
 export const getTasks = async (req, res) => {
