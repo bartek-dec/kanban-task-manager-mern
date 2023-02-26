3e170f000203bce6425b2b22ac748d190ff31a98
bartek dec <decbartek@gmail.com>
2023-02-26 22:19:11 +0100

    added handleTaskChange() in the BoardListItem component

--- client/src/components/BoardListItem.js
+++ client/src/components/BoardListItem.js
@@ -1,12 +1,18 @@
 import {TbLayoutBoardSplit} from 'react-icons/tb';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {setActiveBoard} from "../features/board/boardSlice";
 import {closeSidebarModal} from '../features/sidebarSlice/sidebarSlice';
-import {getTasks} from "../features/task/taskSlice";
+import {getTasks, handleTaskChange} from "../features/task/taskSlice";
 import {NavLink} from "react-router-dom";
+import {useEffect} from "react";
 
 const BoardListItem = ({title, id}) => {
     const dispatch = useDispatch();
+    const {activeBoard} = useSelector((state) => state.board);
+
+    useEffect(() => {
+        dispatch(handleTaskChange({name: 'status', value: activeBoard?.columns?.[0]}));
+    }, [activeBoard]);
 
     const handleClick = () => {
         dispatch(setActiveBoard(id));
